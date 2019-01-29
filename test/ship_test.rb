38bb23e35f5b3c67a0da238d98db6e33469ad90c
Blake Enyart <blake.enyart@gmail.com>
2019-01-29 12:15:55 -0700

    Creates health decreases with hit method and sinks

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -5,34 +5,63 @@ require 'minitest/pride'
 
 class ShipTest < Minitest::Test
 
-  def test_ship_exist
-    ship = Ship.new()
+  def setup
+    @ship = Ship.new("Cruiser", 3)
+  end
 
-    assert_instance_of Ship, ship
+  def test_ship_exist
+
+    assert_instance_of Ship, @ship
+  end
+
+  def test_attributes_return_correctly
+
+    assert_equal "Cruiser", @ship.name
+    assert_equal 3, @ship.length
+  end
+
+  def test_health_decreases_with_hit_method_and_sinks
+    @ship.health
+    @ship.sunk?
+    @ship.hit
+
+    assert_equal 2, @ship.health
+    @ship.hit
+
+    assert_equal 1, @ship.health
+    @ship.hit
+
+    assert_equal true, @ship.sunk?
   end
 end
 
 
-# cruiser = Ship.new("Cruiser", 3)
-#
 # cruiser.name
+# #=> "Cruiser"
 #
 # cruiser.length
+# #=> 3
 #
 # cruiser.health
+# #=> 3
 #
 # cruiser.sunk?
+# #=> false
 #
 # cruiser.hit
 #
 # cruiser.health
+# #=> 2
 #
 # cruiser.hit
 #
 # cruiser.health
+# #=> 1
 #
 # cruiser.sunk?
+# #=> false
 #
 # cruiser.hit
 #
 # cruiser.sunk?
+# #=> true
