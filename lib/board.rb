08f051bebfb390c4e78256eb909e6167c673431d
Rob Stringer <stringer525@gmail.com>
2019-01-30 18:13:32 -0700

    midway commit switching blake from navigation to driver's seat

--- lib/board.rb
+++ lib/board.rb
@@ -1,7 +1,7 @@
 
 
 class Board
-  attr_reader :cells
+  attr_reader :cells, :letters, :numbers
 
   def initialize
     keys = ["A1", "A2", "A3", "A4", "B1", "B2", "B3", "B4", "C1", "C2", "C3", "C4", "D1", "D2", "D3", "D4"]
@@ -14,6 +14,18 @@ class Board
   end
 
   def valid_placement?(ship, placement)
-    ship.length == placement.length
+    split_cell = placement.map { |cell| cell.split(//) }.flatten
+    @letters = []
+    @numbers = []
+
+    for element in split_cell do
+      if split_cell.index(element).even?
+        @letters << element
+      elsif split_cell.index(element).odd?
+        @numbers << element.to_i
+      end
+    end
+    # binding.pry
+    ship.length == placement.length && consecutive
   end
 end
--- test/board_test.rb
+++ test/board_test.rb
@@ -39,18 +39,31 @@ class BoardTest < Minitest::Test
   end
 
   def test_valid_ship_length
+    skip
     assert_equal false, @board.valid_placement?(@cruiser, ["A1", "A2"])
     assert_equal true, @board.valid_placement?(@cruiser, ["A1", "A2", "A3"])
     assert_equal false, @board.valid_placement?(@submarine, ["A1", "A2", "A4"])
   end
 
   def  test_consecutive_cell_placement
+    skip
     assert_equal false,  @board.valid_placement?(@cruiser, ["A1", "A2", "A4"])
     assert_equal false,  @board.valid_placement?(@submarine, ["A1", "C1"])
     assert_equal false,  @board.valid_placement?(@cruiser, ["A3", "A2", "A1"])
     assert_equal false,  @board.valid_placement?(@submarine, ["C1", "B1"])
     assert_equal true, @board.valid_placement?(@cruiser, ["A1", "A2", "A3"])
   end
+
+  def test_consecutive_letters
+    skip
+    assert_equal true,  @board.letters
+    assert_equal true,  @board.letters
+    assert_equal false, @board.letters
+    assert_equal false, @board.letters
+    assert_equal false, @board.letters
+  end
+
+
 end
 
 
