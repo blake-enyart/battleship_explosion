ea4a088feb5f6e23e74ad4081be6c1f58b9105d8
Blake Enyart <blake.enyart@gmail.com>
2019-02-04 12:03:34 -0700

    Refactored board test

--- test/board_test.rb
+++ test/board_test.rb
@@ -6,33 +6,32 @@ require 'minitest/pride'
 require 'pry'
 
 class BoardTest < Minitest::Test
+
   def setup
     @board = Board.new
     @cruiser = Ship.new("Cruiser", 3)
     @submarine = Ship.new("Submarine", 2)
-    # @board.place(@cruiser, ["A1", "A2", "A3"])
-    #
-    # @board.valid_placement?(@submarine, ["A1", "B1"])
-    # binding.pry
   end
 
   def test_board_exists
-    board = Board.new
 
-    assert_instance_of Board, board
+    assert_instance_of Board, @board
   end
 
 
   def test_instantiation_of_board_creates_hash
+
      assert_instance_of Hash, @board.cells
   end
 
-  def test_whether_board_has_16_key_value_pairs
+  def test_whether_board_has_hash_with_16_key_value_pairs
+
     assert_instance_of Hash, @board.cells
     assert_equal 16, @board.cells.length
   end
 
-  def test_key_points_to_cell_object_in_board
+  def test_key_points_to_cell_object_value_in_board
+
     assert_instance_of Cell, @board.cells["A1"]
   end
 
@@ -47,7 +46,9 @@ class BoardTest < Minitest::Test
 
     assert_equal false, @board.verify_ship_length(@cruiser, ["A1", "A2"])
     assert_equal true, @board.verify_ship_length(@cruiser, ["A1", "A2", "A3"])
+
     assert_equal false, @board.verify_ship_length(@submarine, ["A1", "A2", "A4"])
+    assert_equal true, @board.verify_ship_length(@submarine, ["A1", "A2"])
   end
 
   def test_diagonal_placements_are_rejected
@@ -57,17 +58,21 @@ class BoardTest < Minitest::Test
     assert_equal true, @board.valid_placement?(@cruiser, ["A1", "A2", "A3"])
   end
 
-  def test_backwards_and_nonsequential_placements_rejected
+  def test_backwards_and_nonsequential_placements_are_rejected
 
-    assert_equal false,  @board.valid_placement?(@cruiser, ["A31", "A2", "A1"])
     assert_equal false,  @board.valid_placement?(@cruiser, ["A3", "A2", "A1"])
     assert_equal false,  @board.valid_placement?(@submarine, ["A1", "C1"])
     assert_equal true, @board.valid_placement?(@cruiser, ["A1", "A2", "A3"])
   end
 
-  def test_cannot_enter_nothing_or_off_board
+  def test_cannot_place_ships_off_board
 
-    assert_equal false,  @board.valid_placement?(@cruiser, ["A31", "A2", "A1"])
+    assert_equal false,  @board.valid_placement?(@cruiser, ["A3", "A4", "A5"])
+    assert_equal true, @board.valid_placement?(@cruiser, ["A1", "A2", "A3"])
+  end
+
+  def test_cannot_use_empty_placement
+
     assert_equal false,  @board.valid_placement?(@cruiser, [])
   end
 
@@ -76,8 +81,10 @@ class BoardTest < Minitest::Test
 
     assert_instance_of Cell, @board.cells["A1"]
     assert_equal @submarine, @board.cells["A1"].ship
+
     assert_instance_of Cell, @board.cells["A2"]
     assert_equal @submarine, @board.cells["A2"].ship
+
     assert_instance_of Cell, @board.cells["A3"]
     refute_equal @submarine, @board.cells["A3"].ship
   end
@@ -86,8 +93,10 @@ class BoardTest < Minitest::Test
     @board.place(@cruiser, ["A1", "A2", "A3"])
 
     assert_equal true, @board.valid_placement?(@cruiser, ["A1", "A2", "A3"])
+
     assert_equal false, @board.valid_placement?(@submarine, ["A1", "A2"])
     assert_equal false, @board.valid_placement?(@submarine, ["A1", "B1"])
+    assert_equal true, @board.valid_placement?(@submarine, ["B1", "B2"])
   end
 
   def test_render_displays_board_hidden_and_shows_ship_with_true
@@ -95,8 +104,9 @@ class BoardTest < Minitest::Test
     expected = "  1 2 3 4 \nA . . . . \nB . . . . \nC . . . . \nD . . . . \n"
 
     assert_equal expected, @board.render
-    apple = "  1 2 3 4 \nA S S S . \nB . . . . \nC . . . . \nD . . . . \n"
 
-    assert_equal apple, @board.render(true)
+    expected = "  1 2 3 4 \nA S S S . \nB . . . . \nC . . . . \nD . . . . \n"
+
+    assert_equal expected, @board.render(true)
   end
 end
